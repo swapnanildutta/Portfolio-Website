35b06e645d53d1568c2d3ae517e264daad65e0db
CodyJasonBennett <hi@codyb.co>
2019-05-16 12:36:15 -0500

    Optimized placeholders, updated components.

---
+++ src/components/ProjectImage.js
@@ -0,0 +1,195 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import styled, { css, keyframes } from 'styled-components/macro';
+import 'intersection-observer';
+import macbook from '../assets/macbook-large.png';
+
+const prerender = navigator.userAgent === 'ReactSnap';
+
+function ProgressiveImage(props) {
+  const { className, style, reveal, delay = 0, ...rest } = props;
+  const [loaded, setLoaded] = useState(false);
+  const [intersect, setIntersect] = useState(false);
+  const containerRef = useRef();
+
+  const onLoad = useCallback(() => {
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const image = entry.target;
+          setIntersect(true);
+          observer.unobserve(image);
+        }
+      });
+    });
+
+    observer.observe(containerRef.current);
+
+    return function cleanUp() {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <ImageContainer
+      className={className}
+      style={style}
+      ref={containerRef}
+      reveal={reveal}
+      intersect={intersect}
+      loaded={loaded}
+      delay={delay}
+    >
+      {reveal &&
+        <ImageFade intersect={intersect} delay={delay}>
+          <ImageElements
+            delay={delay}
+            onLoad={onLoad}
+            loaded={loaded}
+            intersect={intersect}
+            {...rest}
+          />
+        </ImageFade>
+      }
+      {!reveal &&
+        <ImageElements
+          onLoad={onLoad}
+          loaded={loaded}
+          intersect={intersect}
+          {...rest}
+        />
+      }
+    </ImageContainer>
+  );
+};
+
+function ImageElements(props) {
+  const { onLoad, loaded, intersect, srcSet, placeholder, delay, ...rest } = props;
+  const [showPlaceholder, setShowPlaceholder] = useState(true);
+  const placeholderRef = useRef();
+
+  const purgePlaceholder = useCallback(() => {
+    setShowPlaceholder(false);
+  }, []);
+
+  useEffect(() => {
+    const placeholderElement = placeholderRef.current;
+    placeholderElement.addEventListener('transitionend', purgePlaceholder);
+
+    return function cleanUp() {
+      if (placeholderElement) {
+        placeholderElement.removeEventListener('transitionend', purgePlaceholder);
+      }
+    };
+  }, [purgePlaceholder]);
+
+  return (
+    <React.Fragment>
+      <ImageActual
+        delay={delay}
+        onLoad={onLoad}
+        decoding="async"
+        loaded={loaded}
+        srcSet={!prerender && intersect ? srcSet : null}
+        {...rest}
+      />
+      {showPlaceholder &&
+        <ImagePlaceholder
+          delay={delay}
+          ref={placeholderRef}
+          loaded={loaded}
+          src={placeholder}
+          alt=""
+          role="presentation"
+        />
+      }
+    </React.Fragment>
+  );
+}
+
+const AnimImageReveal = keyframes`
+  0% {
+    transform: scale3d(0, 1, 1);
+    transform-origin: left;
+  }
+  49% {
+    transform: scale3d(1, 1, 1);
+    transform-origin: left;
+  }
+  50% {
+    transform: scale3d(1, 1, 1);
+    transform-origin: right;
+  }
+  100% {
+    transform: scale3d(0, 1, 1);
+    transform-origin: right;
+  }
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+  transform: translate3d(0, 0, 0);
+  display: grid;
+  grid-template-columns: 100%;
+  background-image: url(${macbook});
+  background-size: cover;
+
+  ${props => props.reveal && css`
+    &:before {
+      content: '';
+      background: ${props => props.theme.colorAccent};
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      transform: scale3d(0, 1, 1);
+      transform-origin: left;
+      z-index: 16;
+      animation: ${props.intersect && !prerender && css`
+        ${AnimImageReveal} 1.8s ${props.theme.curveFastoutSlowin} ${props.delay + 200}ms
+      `};
+    }
+  `}
+`;
+
+const ImageFade = styled.div`
+  opacity: ${props => props.intersect ? 1 : 0};
+  transition: opacity 0.4s ease ${props => props.delay + 1000}ms;
+  transform: translate3d(0, 0, 0);
+  position: relative;
+  display: grid;
+  grid-template-columns: 100%;
+`;
+
+const ImagePlaceholder = styled.img`
+  width: 77.62%;
+  height: 78.90%;
+  top: 9.05%;
+  left: 11.35%;
+  transition: opacity 0.4s ease;
+  pointer-events: none;
+  display: block;
+  position: relative;
+  z-index: 1;
+  opacity: ${props => props.loaded ? 0 : 1};
+  grid-column: 1;
+  grid-row: 1;
+`;
+
+const ImageActual = styled.img`
+  width: 77.62%;
+  height: 78.90%;
+  top: 9.05%;
+  left: 11.35%;
+  position: absolute;
+  display: block;
+  opacity: ${props => props.loaded ? 1 : 0};
+  grid-column: 1;
+  grid-row: 1;
+`;
+
+export default React.memo(ProgressiveImage);
---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef } from 'react';
+import styled, { css } from 'styled-components/macro';
+import lottie from 'lottie-web/build/player/lottie_light.min';
+import nightModeAnimation from '../data/NightModeIconData.json';
+import { media, rgba } from '../utils/StyleUtils';
+
+export default function ThemeToggle({ themeId, toggleTheme, isMobile, ...rest }) {
+  const initThemeId = useRef(themeId);
+  const lottieContainerRef = useRef();
+  const lottieAnimRef = useRef();
+
+  useEffect(() => {
+    lottieAnimRef.current = lottie.loadAnimation({
+      container: lottieContainerRef.current,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      animationData: nightModeAnimation,
+    });
+
+    const duration = lottieAnimRef.current.totalFrames - 1;
+    lottieAnimRef.current.goToAndStop(initThemeId.current === 'dark' ? duration : 0, true);
+  }, []);
+
+  useEffect(() => {
+    lottieAnimRef.current.setDirection(themeId === 'dark' ? 1 : -1);
+    lottieAnimRef.current.play();
+  }, [themeId]);
+
+  return (
+    <ThemeToggleButton aria-label="Toggle theme" onClick={toggleTheme} isMobile={isMobile} {...rest}>
+      <div ref={lottieContainerRef} />
+    </ThemeToggleButton>
+  );
+}
+
+const ThemeToggleButton = styled.button`
+  border: 0;
+  margin: 0;
+  padding: 6px;
+  background: none;
+  position: fixed;
+  cursor: pointer;
+  z-index: 2048;
+  width: 48px;
+  height: 48px;
+  clip-path: ${props => props.theme.clipPath(8)};
+  transition: background 0.3s ease;
+
+  top: ${props => props.theme.spacingOuter.numDesktop - 8}px;
+  right: ${props => props.theme.spacingOuter.numDesktop - 8}px;
+
+  @media (max-width: ${media.tablet}) {
+    top: ${props => props.isMobile ? 'unset' : `${props.theme.spacingOuter.numTablet - 8}px`};
+    right: ${props => props.isMobile ? '30px' : `${props.theme.spacingOuter.numTablet - 8}px`};
+  }
+
+  ${props => props.isMobile && css`
+    top: unset;
+    bottom: 30px;
+  `}
+
+  &:hover,
+  &:focus,
+  &:active {
+    background: ${props => rgba(props.theme.colorText, 0.1)};
+    outline: none;
+  }
+
+  svg path {
+    transition-property: fill, stroke;
+    transition-timing-function: ease;
+    transition-duration: 0.4s;
+  }
+
+  svg g > path {
+    stroke: ${props => props.theme.colorText};
+  }
+
+  svg g[mask] path {
+    fill: ${props => props.theme.colorText};
+    stroke: none;
+  }
+`;
