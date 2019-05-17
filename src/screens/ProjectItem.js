8c1be671e74fc93f658fef60c092ff0ea2a663c7
CodyJasonBennett <hi@codyb.co>
2019-05-17 16:14:13 -0500

    Inlined macbook template within laptop container.

--- src/screens/ProjectItem.js
+++ src/screens/ProjectItem.js
@@ -1,120 +1,13 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import styled, { css, keyframes } from 'styled-components/macro';
+import React from 'react';
+import styled, { css } from 'styled-components/macro';
 import { Transition } from 'react-transition-group';
 import { media, rgba } from '../utils/StyleUtils';
 import { RouterButton, LinkButton } from '../components/Button';
-import 'intersection-observer';
+import ProgressiveImage from '../components/ProgressiveImage';
 import macbook from '../assets/macbook.png';
 import phone from '../assets/phone.png';
 import phoneLarge from '../assets/phone-large.png';
 import phonePlaceholder from '../assets/phone-placeholder.png';
-
-const prerender = navigator.userAgent === 'ReactSnap';
-
-function ProjectImage(props) {
-  const { className, style, reveal, delay = 0, ...rest } = props;
-  const [loaded, setLoaded] = useState(false);
-  const [intersect, setIntersect] = useState(false);
-  const containerRef = useRef();
-
-  const onLoad = useCallback(() => {
-    setLoaded(true);
-  }, []);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          const image = entry.target;
-          setIntersect(true);
-          observer.unobserve(image);
-        }
-      });
-    });
-
-    observer.observe(containerRef.current);
-
-    return function cleanUp() {
-      observer.disconnect();
-    };
-  }, []);
-
-  return (
-    <ImageContainer
-      className={className}
-      style={style}
-      ref={containerRef}
-      reveal={reveal}
-      intersect={intersect}
-      loaded={loaded}
-      delay={delay}
-    >
-      {reveal &&
-        <ImageFade intersect={intersect} delay={delay}>
-          <ImageElements
-            delay={delay}
-            onLoad={onLoad}
-            loaded={loaded}
-            intersect={intersect}
-            {...rest}
-          />
-        </ImageFade>
-      }
-      {!reveal &&
-        <ImageElements
-          onLoad={onLoad}
-          loaded={loaded}
-          intersect={intersect}
-          {...rest}
-        />
-      }
-    </ImageContainer>
-  );
-};
-
-function ImageElements(props) {
-  const { onLoad, loaded, intersect, srcSet, placeholder, delay, ...rest } = props;
-  const [showPlaceholder, setShowPlaceholder] = useState(true);
-  const placeholderRef = useRef();
-
-  const purgePlaceholder = useCallback(() => {
-    setShowPlaceholder(false);
-  }, []);
-
-  useEffect(() => {
-    const placeholderElement = placeholderRef.current;
-    placeholderElement.addEventListener('transitionend', purgePlaceholder);
-
-    return function cleanUp() {
-      if (placeholderElement) {
-        placeholderElement.removeEventListener('transitionend', purgePlaceholder);
-      }
-    };
-  }, [purgePlaceholder]);
-
-  return (
-    <React.Fragment>
-      <ImageActual
-        delay={delay}
-        onLoad={onLoad}
-        decoding="async"
-        loaded={loaded}
-        srcSet={!prerender && intersect ? srcSet : null}
-        {...rest}
-      />
-      {showPlaceholder &&
-        <ImagePlaceholder
-          delay={delay}
-          ref={placeholderRef}
-          loaded={loaded}
-          src={placeholder}
-          alt=""
-          role="presentation"
-        />
-      }
-    </React.Fragment>
-  );
-}
 
 function ProjectItem(props) {
   const {
@@ -195,88 +88,6 @@ function ProjectItem(props) {
     </ProjectItemSection>
   );
 };
-
-const AnimImageReveal = keyframes`
-  0% {
-    transform: scale3d(0, 1, 1);
-    transform-origin: left;
-  }
-  49% {
-    transform: scale3d(1, 1, 1);
-    transform-origin: left;
-  }
-  50% {
-    transform: scale3d(1, 1, 1);
-    transform-origin: right;
-  }
-  100% {
-    transform: scale3d(0, 1, 1);
-    transform-origin: right;
-  }
-`;
-
-const ImageContainer = styled.div`
-  position: relative;
-  transform: translate3d(0, 0, 0);
-  display: grid;
-  grid-template-columns: 100%;
-  background-image: url(${macbook});
-  background-size: cover;
-
-  ${props => props.reveal && css`
-    &:before {
-      content: '';
-      background: ${props => props.theme.colorAccent};
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      transform: scale3d(0, 1, 1);
-      transform-origin: left;
-      z-index: 16;
-      animation: ${props.intersect && !prerender && css`
-        ${AnimImageReveal} 1.8s ${props.theme.curveFastoutSlowin} ${props.delay + 200}ms
-      `};
-    }
-  `}
-`;
-
-const ImageFade = styled.div`
-  opacity: ${props => props.intersect ? 1 : 0};
-  transition: opacity 0.4s ease ${props => props.delay + 1000}ms;
-  transform: translate3d(0, 0, 0);
-  position: relative;
-  display: grid;
-  grid-template-columns: 100%;
-`;
-
-const ImagePlaceholder = styled.img`
-  width: 77.62%;
-  height: 78.90%;
-  top: 9.05%;
-  left: 11.35%;
-  transition: opacity 0.4s ease;
-  pointer-events: none;
-  display: block;
-  position: relative;
-  z-index: 1;
-  opacity: ${props => props.loaded ? 0 : 1};
-  grid-column: 1;
-  grid-row: 1;
-`;
-
-const ImageActual = styled.img`
-  width: 77.62%;
-  height: 78.90%;
-  top: 9.05%;
-  left: 11.35%;
-  position: absolute;
-  display: block;
-  opacity: ${props => props.loaded ? 1 : 0};
-  grid-column: 1;
-  grid-row: 1;
-`;
 
 const ProjectItemContent = styled.div`
   width: 100%;
@@ -527,9 +338,14 @@ const ProjectItemButton = styled.div`
   `}
 `;
 
-const ProjectItemImageLaptop = styled(ProjectImage)`
+const ProjectItemImageLaptop = styled(ProgressiveImage)`
   width: 862px;
   height: 531px;
+  padding: 5.5% 10.9% 7.3% 11.4%;
+  display: flex;
+  justify-content: center;
+  background-image: url(${macbook});
+  background-size: cover;
   transition-property: transform, opacity;
   transition-duration: 1s;
   transition-delay: 0.4s;
@@ -538,6 +354,10 @@ const ProjectItemImageLaptop = styled(ProjectImage)`
   opacity: 0;
   position: relative;
   right: -140px;
+
+  img {
+    box-shadow: 0 0 0 2px #1C1C1C;
+  }
 
   ${props => props.status === 'entered' && css`
     transform: translate3d(0, 0, 0);
@@ -563,6 +383,10 @@ const ProjectItemImageLaptop = styled(ProjectImage)`
     height: 258px;
     margin-bottom: 120px;
     right: 0;
+
+    img {
+      box-shadow: 0 0 0 1px #1C1C1C;
+    }
   }
 
   @media (max-width: ${media.mobile}) {
@@ -613,7 +437,7 @@ const ProjectItemPhone = styled.div`
   `}
 `;
 
-const ProjectItemPhoneFrame = styled(ProjectImage)`
+const ProjectItemPhoneFrame = styled(ProgressiveImage)`
   position: absolute;
   width: 414px;
   height: 721px;
@@ -624,7 +448,7 @@ const ProjectItemPhoneFrame = styled(ProjectImage)`
   }
 `;
 
-const ProjectItemPhoneImage = styled(ProjectImage)`
+const ProjectItemPhoneImage = styled(ProgressiveImage)`
   box-shadow: 0 0 0 2px #1C1C1C;
   position: relative;
   top: -14px;
